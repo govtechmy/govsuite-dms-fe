083b579180add3dd8c594946a302a663c8c61712
syarqawi12 <syarqawi@water.gov.my>
2026-06-09 09:55:22 +0800

    RingkasanEksekutif

--- src/components/Homepage/RingkasanEksekutif.tsx
+++ src/components/Homepage/RingkasanEksekutif.tsx
@@ -1,67 +1,68 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../shared/SelectMydsFix";
 
-export default function RingkasanEksekutif({ Tahun }: { Tahun: string[] }) {
-  return (
-    <div >
-      <h1 className="text-heading-3xs font-heading font-semibold">Selamat Datang,</h1>
-      <p className="text-body-md font-normal text-XL-400 mb-4">
-        Mohd Muzakkir Zamani Bin Fairuzzaki
-      </p>
-      <div className="text-body-md font-semibold text-txt-black-900">Ringkasan Eksekutif</div>
-      <div className="flex justify-between items-center mb-3">
-      <div className="text-body-sm font-normal text-txt-black-500 pt-1 pb-3">
-        Berikut adalah status dokumen terkini.
-      </div>
-        <Select size={'small'} variant="outline">
-          <SelectTrigger>
-            <SelectValue label="Tahun" placeholder="2026" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="Tahun">2026</SelectItem>
-            {Tahun.map((TahunValue) => (
-              <SelectItem key={TahunValue} value={TahunValue}>
-                {TahunValue}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
-      
-<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-6 w-full">
-  
-  {/* KAD 1: Jumlah Dokumen */}
-  <a 
-  href="ms/katalog-dokumen"
-  className="relative overflow-hidden bg-bg-primary-50 border-l-[10px] border-l-primary-200 rounded-lg h-[108px] w-full flex flex-col justify-center items-center gap-3">
-  <div className="text-5xl font-semibold text-txt-black-900">
-    821
-  </div>
-  <div className="text-body-m font-medium text-txt-black-700">
-    Jumlah Dokumen
-  </div>
-</a>
+type Card = { label: string; value: number; bgColor: string; borderColor: string };
 
-  {/* KAD 2: Perlukan Kelulusan */}
-  <div className="relative overflow-hidden bg-bg-warning-50 border-l-[10px] border-l-bg-warning-200 rounded-lg h-[108px] w-full flex flex-col justify-center items-center gap-3">
-    <div className="text-5xl font-semibold text-txt-black-900">
-      42
-    </div>
-    <div className="text-body-m font-medium text-txt-black-700">
-      Perlukan Kelulusan
-    </div>
-  </div>
+const variantMapping = ['JDokumen', 'Kelulusan', 'taklulus'] as const;
 
-  {/* KAD 3: Dokumen Ditolak */}
-  <div className="relative overflow-hidden bg-bg-danger-50 border-l-[10px] border-l-bg-danger-200 rounded-lg h-[108px] w-full flex flex-col justify-center items-center gap-3">
-    <div className="text-5xl font-semibold text-txt-black-900">
-      10
-    </div>
-    <div className="text-body-m font-medium text-txt-black-700">
-      Dokumen Ditolak
-    </div>
-  </div>
+export default function RingkasanEksekutif({ Tahun, card }: { Tahun: string[]; card: Card[] }) {
+    return (
+        <div>
+            <h1 className="text-heading-3xs font-heading font-semibold">Selamat Datang,</h1>
+            <p className="text-body-md font-normal text-XL-400 mb-4">Mohd Muzakkir Zamani Bin Fairuzzaki</p>
+            <div className="text-body-md font-semibold text-txt-black-900">Ringkasan Eksekutif</div>
 
-</div>
-    </div>
-    )
-    }
+            <div className="flex justify-between items-center mb-3">
+                <div className="text-body-sm font-normal text-txt-black-500 pt-1 pb-3">Berikut adalah status dokumen terkini.</div>
+
+                <Select size={"small"} variant="outline">
+                    <SelectTrigger>
+                        <SelectValue label="Tahun" placeholder="2026" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="Tahun">2026</SelectItem>
+                        {Tahun.map((TahunValue) => (
+                            <SelectItem key={TahunValue} value={TahunValue}>
+                                {TahunValue}
+                            </SelectItem>
+                        ))}
+                    </SelectContent>
+                </Select>
+            </div>
+
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-6 w-full">
+                {card.map((cardItem, index) => {
+                    const variant = variantMapping[index];
+
+                    return (
+                        <div key={index}>
+
+                            {variant === 'JDokumen' && (
+                                <div className={`relative overflow-hidden ${cardItem.bgColor} border-l-[10px] ${cardItem.borderColor} rounded-lg h-[108px] w-full flex flex-col justify-center items-center gap-3`}>
+                                    <div className="text-5xl font-semibold text-txt-black-900">{cardItem.value}</div>
+                                    <div className="text-body-m font-medium text-txt-black-700">{cardItem.label}</div>
+                                </div>
+                            )}
+
+                            {variant === 'Kelulusan' && (
+                                <div className={`relative overflow-hidden ${cardItem.bgColor} border-l-[10px] ${cardItem.borderColor} rounded-lg h-[108px] w-full flex flex-col justify-center items-center gap-3`}>
+                                    <div className="text-5xl font-semibold text-txt-black-900">{cardItem.value}</div>
+                                    <div className="text-body-m font-medium text-txt-black-700">{cardItem.label}</div>
+                                </div>
+                            )}
+
+              
+                            {variant === 'taklulus' && (
+                                <div className={`relative overflow-hidden ${cardItem.bgColor} border-l-[10px] ${cardItem.borderColor} rounded-lg h-[108px] w-full flex flex-col justify-center items-center gap-3`}>
+                                    <div className="text-5xl font-semibold text-txt-black-900">{cardItem.value}</div>
+                                    <div className="text-body-m font-medium text-txt-black-700">{cardItem.label}</div>
+                                </div>
+                            )}
+                        </div>
+                    );
+                })}
+            </div>
+
+        </div>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,11 @@ import RingkasanEksekutif  from '@/components/Homepage/RingkasanEksekutif'
 export default function HomePage() {
 
   const Tahun= [ '2025', '2024']
+  const card = [
+    { label: 'Jumlah Dokumen', value: 821, bgColor: 'bg-bg-primary-50', borderColor: 'border-l-primary-200' },
+    { label: 'Perlukan Kelulusan', value: 42, bgColor: 'bg-bg-warning-50', borderColor: 'border-l-bg-warning-200' },
+    { label: 'Dokumen tidak diluluskan', value: 10, bgColor: 'bg-bg-danger-50', borderColor: 'border-l-bg-danger-200' }
+];
 
   const unit = ['unit k ', 'unit l']
   const data = [
@@ -51,7 +56,7 @@ export default function HomePage() {
   return (
     <>
       <RightSidePageLayoutWrapper className="border-b border-otl-gray-200">
-      <RingkasanEksekutif Tahun={Tahun} />
+      <RingkasanEksekutif Tahun={Tahun} card={card} />
       </RightSidePageLayoutWrapper>
       <RightSidePageLayoutWrapper className="border-b border-otl-gray-200">
         <TrendKekerapanDokumen unit={unit} data={data} />
