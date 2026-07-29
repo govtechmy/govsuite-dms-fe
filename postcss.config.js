72ba5d1fc61efbce0ee3ca8f4c12a3126d612590
harrisazmi <azmi.harris@gmail.com>
2026-07-29 11:43:21 +0800

    hotfix : govsuitedms-(384) : Local CSS lock, myds goes out for google font

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+// @govtechmy/myds-style bundles a remote `@import url(https://fonts.googleapis.com/...)`
+// for Inter/Poppins. This app self-hosts those fonts (see src/index.css), so that
+// remote import is stripped here to avoid any outbound network request at runtime.
+const stripGoogleFontsImport = () => ({
+  postcssPlugin: 'strip-google-fonts-import',
+  AtRule: {
+    import: (atRule) => {
+      if (/fonts\.googleapis\.com/.test(atRule.params)) {
+        atRule.remove()
+      }
+    },
   },
+})
+stripGoogleFontsImport.postcss = true
+
+export default {
+  plugins: [stripGoogleFontsImport, tailwindcss(), autoprefixer()],
 }
