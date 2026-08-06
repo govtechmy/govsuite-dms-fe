dfbd1d9ad77293d100fd637edb5ab484abad9074
harrisazmi <azmi.harris@gmail.com>
2026-08-06 16:06:19 +0800

    feature : govsuitedms-(410): carian kandungan minyak changes

--- src/components/page/CarianDokumen/SearchBarCarianDokumen.tsx
+++ src/components/page/CarianDokumen/SearchBarCarianDokumen.tsx
@@ -107,7 +107,7 @@ export default function SearchBarCarianDokumen({ className }: SearchBarCarianDok
           <div className="w-full border border-otl-primary-200 rounded-lg bg-bg-primary-50 px-3 py-4 flex flex-col gap-2 text-body-sm">
             <div className="text-txt-black-700 font-semibold">Tips membuat carian</div>
             <div className="text-txt-black-500">
-              <p> Carian kata kunci, contohnya minyak</p>
+              <p> Carian kata kunci, contohnya: minyak</p>
               {/* <p>Carian kata kunci pelbagai, contohnya minyak, banjir</p> */}
             </div>
           </div>
