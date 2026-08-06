7b570f84e90fada966e7f6206723759affc72520
harrisazmi <azmi.harris@gmail.com>
2026-08-06 15:21:52 +0800

    feature : govsuitedms-(404) : search by name to carian nama dokumen

--- src/components/page/KatalogDokumen/SearchBarKatalogDokumen.tsx
+++ src/components/page/KatalogDokumen/SearchBarKatalogDokumen.tsx
@@ -70,7 +70,7 @@ export default function SearchBarKatalogDokumen() {
     >
       <SearchBarInputContainer>
         <SearchBarInput
-          placeholder="Search by name"
+          placeholder="Carian nama dokumen"
           value={query}
           onValueChange={setQuery}
           onFocus={() => setHasFocus(true)}
