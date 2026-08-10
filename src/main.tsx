3d6890f08b1c0302ba9ce1c8b4ec0694d9c1aa22
harrisazmi <azmi.harris@gmail.com>
2026-08-10 12:00:33 +0800

    feature : govsuitedms-(426) : injection of available env in env.js

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { getEnv } from './config/runtimeEnv'
 import { initializeAuthAxios } from './services/http'
 import { useAuthStore } from './store/AuthStore'
+
+document.title = getEnv('VITE_APP_NAME', 'GovSuite DMS')
 
 if (import.meta.env.DEV) {
   console.info('[env]', {
