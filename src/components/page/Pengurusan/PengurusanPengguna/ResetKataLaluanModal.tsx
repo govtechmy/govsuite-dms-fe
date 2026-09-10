469103257e296d2ba4e22d72ee78eba09f2afe1e
harrisazmi <azmi.harris@gmail.com>
2026-09-10 14:03:35 +0800

    hotfix : prettier

--- src/components/page/Pengurusan/PengurusanPengguna/ResetKataLaluanModal.tsx
+++ src/components/page/Pengurusan/PengurusanPengguna/ResetKataLaluanModal.tsx
@@ -86,11 +86,7 @@ export default function ResetKataLaluanModal({ pengguna, onSuccess }: ResetKataL
   return (
     <Dialog open={open} onOpenChange={handleOpenChange}>
       <DialogTrigger>
-        <Button
-          variant="danger-outline"
-          size="small"
-          disabled={pengguna.disabledResetPassword}
-        >
+        <Button variant="danger-outline" size="small" disabled={pengguna.disabledResetPassword}>
           <LockIcon />
           Reset
         </Button>
